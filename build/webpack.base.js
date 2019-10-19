b172f22ec65e238774bcf624ebdfb4ea34330637
金志强 <haoxihaoderen@163.com>
2019-10-19 16:07:12 +0800

    feat:添加htmlwebpackplugin和cleanwebpackplugin配置

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,8 +1,11 @@
 const entryPath = '../src/index.js';
 const outputPath = '../dist';
+const htmlPath = '../public/index.html';
+const faviconPath = '../public/favicon.ico';
 
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
 	mode: 'production',
 	entry: {
@@ -10,14 +13,21 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, outputPath),
-		filename: 'js/[name].[hash:12].js',
-		// filename: 'static/js/[name].[hash:8].js',
+		filename: 'js/[name].[hash:8].js',
     // chunkFilename: 'static/js/[name].[chunkhash:8].js',
     publicPath: '/'
 	},
 	module: {
 		rules: [
 			
-		]
-	}
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, htmlPath),
+			favicon: path.resolve(__dirname, faviconPath),
+			filename: 'index.[hash:8].html'
+		})
+	]
 }
