5aa6eaa5c039f77e95268ae6fc662536973ea567
金志强 <haoxihaoderen@163.com>
2019-10-21 08:58:20 +0800

    feat:添加happypack和thread-loader加快编译速度

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,41 +1,45 @@
 const htmlPath = '../public/index.html';
 const faviconPath = '../public/favicon.ico';
 const path = require('path');
+const HappyPack = require('happypack');
+const HappyThreadPool = HappyPack.ThreadPool({ size: 5 });
 
 const WebpackMerge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.base');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 module.exports = WebpackMerge(webpackBaseConfig, {
 	mode: 'production',
 	module: {
 		rules: [
 			{
-				test: /.vue$/,
-				loader: 'vue-loader'
+				test: /\.vue$/,
+				use: [
+					'thread-loader',
+					'vue-loader'
+				]
 			},
 			{
         test: /\.css$/,
         use: [
 					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'postcss-loader',
+					'happypack/loader?id=css'
         ]
       },
 			{
         test: /\.scss$/,
         use: [
+					//TODO sass-loader and happypack
 					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'postcss-loader',
-          'sass-loader'
+					'happypack/loader?id=scsss'
+					// 'css-loader', 'postcss-loader', 'sass-loader'
         ]
 			},
 			{
         test: /\.js$/,
-        use: 'babel-loader',
+        use: 'happypack/loader?id=babel',
         exclude: /node_modules/
       },
 		],
@@ -43,6 +47,21 @@ module.exports = WebpackMerge(webpackBaseConfig, {
 	plugins: [
 		new CleanWebpackPlugin(),
 		new VueLoaderPlugin(),
+		new HappyPack({
+      id: 'scsss',
+      threadPool: HappyThreadPool,
+			loaders: ['css-loader', 'postcss-loader', 'sass-loader'],
+		}),
+		new HappyPack({
+      id: 'css',
+      threadPool: HappyThreadPool,
+			loaders: ['css-loader', 'postcss-loader'],
+    }),
+		new HappyPack({
+			id: 'babel',
+      loaders: ['babel-loader'],
+      threadPool: HappyThreadPool,
+		}),
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, htmlPath),
 			favicon: path.resolve(__dirname, faviconPath),
@@ -50,8 +69,6 @@ module.exports = WebpackMerge(webpackBaseConfig, {
 			minify: true
 		}),
 		new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: "css/[name].[hash:8].css",
       chunkFilename: "[id].css"
     })
